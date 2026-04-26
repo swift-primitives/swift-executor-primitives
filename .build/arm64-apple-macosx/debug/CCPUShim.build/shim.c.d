3e3485092a24e6d50366138b119967f7ac5a9b31
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-04-26 17:55:17 +0200

    Save progress: 2026-04-26

--- .build/arm64-apple-macosx/debug/CCPUShim.build/shim.c.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/coen/Developer/swift-primitives/swift-cpu-primitives/Sources/CCPUShim/include/module.modulemap \
-  /Users/coen/Developer/swift-primitives/swift-cpu-primitives/Sources/CCPUShim/shim.c \
-  /Users/coen/Developer/swift-primitives/swift-cpu-primitives/Sources/CCPUShim/include/shim.h
--- .build/arm64-apple-macosx/debug/_Shims.build/shims.c.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX26.4.sdk/usr/include/Darwin.modulemap \
-  /Users/coen/Developer/swift-primitives/swift-numeric-primitives/Sources/_Shims/shims.c \
-  /Users/coen/Developer/swift-primitives/swift-numeric-primitives/Sources/_Shims/include/shims.h
